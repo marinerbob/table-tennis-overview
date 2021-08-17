d3fbfe96a0708e062cd2428af3024969d9f389da
marinerbob <robobson@mail.ru>
2021-08-17 12:39:49 +0300

    Initial commit #2
    Added basic css ground and webpack config boilerplate

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
+
+
 module.exports = {
   reactStrictMode: true,
+  webpack: config => {
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        styles: path.resolve(__dirname, 'styles')
+    };
+
+    return config;
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import "@fontsource/roboto";
+
+import { CssBaseline } from "@material-ui/core";
+import "styles/globals.scss";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
